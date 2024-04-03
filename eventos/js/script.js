36fd4990b935658fb9474f12767ea23d41752dda
EDUARDO DIAS DA SILVA <eduardo.dsilva17@senacsp.edu.br>
2024-04-03 17:11:16 -0300

    btn-js

---
+++ eventos/js/script.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector("#my-button")
+btn.addEventListener("click", function () {
+    console.log("Clicou!")
+})
+
+// test
+const secondBtn = document.querySelector("#btn")
+
+function imprimirMensagem() {
+    console.log("teste")
+}
+
+secondBtn.addEventListener("click", imprimirMensagem)
+const thirdBtn = document.querySelector("#other-btn")
+thirdBtn.addEventListener("click", () => {
+    console.log("Evento removido")
+    secondBtn.removeEventListener("click", imprimirMensagem)
+})
---
+++ funcoes_e_arrays/js/script.js
@@ -0,0 +1,120 @@
+// function minhaFuncao(){
+//     console.log("testando")
+// }
+
+// minhaFuncao()
+
+// const minhaFuncaoEmVariavel= function(txt){
+//     console.log(`Imprimindo: ${txt}`)
+// }
+
+// minhaFuncaoEmVariavel("sapato")
+// minhaFuncaoEmVariavel("colher")
+// minhaFuncaoEmVariavel(5+5)
+
+// const a = 10;
+// const b = 20;
+// const c = 30;
+// const d = 40;
+
+// function soma(n1, n2){
+//     console.log(`A soma entre ${n1} e ${n2} é = ${n1 + n2}`)
+// }
+
+// soma(a,b)
+// soma(c,b)
+// soma(d,a)
+
+// function raizQuadrada (n1){
+//     console.log(n1 * n1)
+    
+// }
+// raizQuadrada(2)
+
+// function raizQuadrada (n1){
+//     return n1 * n1
+    
+// }
+// console.log(raizQuadrada(4))
+
+// const raizquadrada = (n1) =>{
+//     return Math. sqrt(n1)
+    
+// }
+// console.log(raizquadrada(4))
+
+// const testeArrow = ( ) => {
+//     console.log("Essa é uma arrow function")
+// }
+
+// testeArrow()
+
+// const parOuImpar = (n) => {
+//     if(n % 2 === 0){
+//         console.log("par")
+//         return
+//     }
+//     console.log("Impar")
+    
+// }
+// parOuImpar(3)
+
+// const list = [1, 2, 3, 4, 5]
+
+// console.log(list)
+// console.log(typeof list)
+// console.log(list.length)
+
+
+// const list2 = ["Fusca", "Variant", "Chevete", "Golf", "Opala", "Brasilia"]
+// // console.log(list2.length);
+// for(let i = 0; i < list2.length; i++){
+//     console.log(`Elemento da lista2: ${list2[i]}`)
+// }
+
+// DESAFIO
+// const cores = ["Azul", "Amarelo", "Verde", "Vermelho", "Branco"]
+// for(let i = 0; i < cores.length; i++){
+//     console.log(`Os elementos da lisa de core: ${cores[i]}`)
+// }
+// console.log(typeof cores)
+
+// const nums = [1, 2, 3, 4, 5]
+// nums.forEach((n) =>{
+//     console.log(`O número atual é: ${n}`)
+// })
+
+// const numbers = [5, 12, 4, 22];
+// const otherNumber = [1, 2, 3];
+
+// const allNumbers = numbers.concat(otherNumber);
+// console.log(allNumbers)
+
+// const text = "algum texto"
+// const text2 = 'EITA JOVEM'
+// console.log(text.toUpperCase())
+// console.log(text2.toLocaleLowerCase())
+
+// const array = ["a", "b", "c"]
+// array.push("d")
+
+// console.log(array)
+// console.log(array.length)
+
+// array.pop()
+// const itemRemovido = array.pop()
+// console.log(itemRemovido)
+// console.log(array)
+// console.log(array.length)
+
+// array.push("dá", "para", "inserir", "varios")
+// console.log(array)
+
+// const letters = ["a", "b", "c"]
+// const letter = letters.shift()
+// console.log(letter)
+// console.log(letters)
+
+// latters.unshift("z", "x", "y")
+// latters.unshift("p")
+// console.log(letters)
